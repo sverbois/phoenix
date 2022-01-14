0bfdc6319e651221baccf9c56592f8c3de87165d
Sébastien Verbois <sebastien.verbois@imio.be>
2022-01-14 21:37:35 +0100

    Add inscription_view

--- coaching/urls.py
+++ coaching/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "coaching"
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("dashboard", views.dashboard_view, name="dashboard"),
+    path("dashboard", views.DashboardView.as_view(), name="dashboard"),
     path("courses", views.CoursesView.as_view(), name="courses"),
     path("courses/<int:pk>/", views.CourseView.as_view(), name="course"),
+    path("inscription", views.inscription_view, name="inscription"),
 ]
--- coaching/views.py
+++ coaching/views.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views import generic
 
 from .models import Course
+from .models import Inscription
 
 
 def home_view(request):
@@ -10,10 +18,22 @@ def home_view(request):
     return render(request, "coaching/home.html", context)
 
 
-@login_required
-def dashboard_view(request):
-    context = {}
-    return render(request, "coaching/dashboard.html", context)
+class DashboardView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        self.request = request
+        context = {"next_sessions": self.get_next_sessions()}
+        return render(request, "coaching/dashboard.html", context)
+
+    def get_next_sessions(self):
+        now = datetime.now()
+        current_user = self.request.user
+        inscriptions = (
+            Inscription.objects.filter(user=current_user)
+            .filter(session__start__gt=now)
+            .order_by("session__start")
+        )
+        return [ins.session for ins in inscriptions]
 
 
 class CoursesView(generic.ListView):
@@ -28,3 +48,21 @@ class CoursesView(generic.ListView):
 class CourseView(generic.DetailView):
     model = Course
     template_name = "coaching/course.html"
+
+
+@login_required
+def inscription_view(request):
+    user_id = request.user.id
+    if request.method == "POST":
+        session_id = request.POST["session_id"]
+        has_inscription = (
+            Inscription.objects.filter(user_id=user_id)
+            .filter(session_id=session_id)
+            .count()
+        )
+        if "add" in request.POST and not has_inscription:
+            new_inscription = Inscription(user_id=user_id, session_id=session_id)
+            new_inscription.save()
+        if "delete" in request.POST and has_inscription:
+            pass
+    return HttpResponseRedirect(reverse("coaching:dashboard"))
